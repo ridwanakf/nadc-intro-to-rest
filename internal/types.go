eec5a8784cc0805c784e24764ff67a90b6b28bab
ridwanakf <ridwan.afwan.k.f@gmail.com>
2020-02-29 00:26:25 +0700

    update book struct

--- internal/types.go
+++ internal/types.go
@@ -18,8 +18,9 @@ type Handler struct {
 
 // Book struct for database query
 type Book struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	Stock  int64  `json:"stock"`
+	ID       int     `json:"book_id" db:"book_id"`
+	Title    string  `json:"book_title" db:"book_name"`
+	Author   string  `json:"book_author_name" db:"book_author_name"`
+	Category string  `json:"book_category" db:"book_category"`
+	Rate     float32 `json:"book_rate" db:"book_category"`
 }
